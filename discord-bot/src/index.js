6f44a78c1224d49b0d4d260480364b1ffed6af26
Aspektrix <chris.sav@outlook.com>
2022-05-10 13:25:56 +0100

    Majority of files written

---
+++ discord-bot/src/index.js
@@ -0,0 +1,51 @@
+require('dotenv').config();
+
+const { Client, MessageEmbed } = require('discord.js');
+const tiktok = require('tiktok-scraper');
+const urlRegexp = /(https?:\/\/[^ ]*)/;
+
+const client = new Client({
+    intents: ['GUILDS','GUILD_MESSAGES']
+})
+
+const resolveId = async () => (await tiktok.getUserProfileInfo(process.env.TIKTOK_ACCOUNT)).userId;
+
+client.on('ready', async() => {
+    const tiktokChannel = await client.channels.fetch(process.env.NOTIFICATION_CHANNEL_ID);
+    const userID = await resolveId();
+    const user = await tiktok.user(userID);
+
+    setInterval(async() => {
+        const { collector: newPosts } = user;
+        if (newPosts.length === 0) return;
+
+        const post = newPosts[0];
+        const author = post.authorMeta.nickName;
+        const link = post.webVideoUrl;
+
+        let ifAlready = [...(await tiktokChannel.messages.fetch({ limit: 1 })).values()];
+        if (ifAlready.length > 0) ifAlready = ifAlready[0].content.match(urlRegexp);
+        if (ifAlready != null) ifAlready = ifAlready[1];
+        if (ifAlready == link) return;
+
+        const embed = new MessageEmbed()
+            .setAuthor({ name: author, iconURL: post.authorMeta.avatar })
+            .setTitle(post.text)
+            .setImage(post.covers.default)
+            .setColor('#4f69c9')
+            .setTimestamp()
+            .setFooter({ text: `Followers: ${new Intl.NumberFormat().format(post.authorMeta.fans)} | Videos: ${new Intl.NumberFormat().format(post.authorMeta.video)} | Likes: ${new Intl.NumberFormat().format(post.authorMeta.heart)}` });
+
+        tiktokChannel.send({
+            content: process.env.NEW_POST.replace(`{url}`,link),
+            embeds: [embed]
+        });
+    }, 15000)
+})
+
+client
+    .on('log', console.log)
+    .on('debug', console.log);
+
+client.login(process.env.DISCORDJS_BOT_TOKEN);
+
